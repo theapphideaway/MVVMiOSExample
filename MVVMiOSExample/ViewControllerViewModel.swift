01e416a386cafd6c538d7e894d620fc619ded2f0
ianschoenrock <theapphideaway@gmail.com>
2019-03-06 22:04:22 -0500

    Initial Commit

---
+++ MVVMiOSExample/ViewControllerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerViewModel.swift
+//  MVVMiOSExample
+//
+//  Created by ian schoenrock on 3/6/19.
+//  Copyright © 2019 ian schoenrock. All rights reserved.
+//
+
+import Foundation
